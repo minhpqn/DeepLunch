5b9bfa71847c13f81fdcd1eb6ddc0d88ed5fec8c
Pham Quang Nhat Minh <nhatminhk47ca@yahoo.com>
2017-01-16 15:44:31 +0700

    Added Keras Examples

---
+++ KerasExamples/imdb/imdb_plot.py
@@ -0,0 +1,26 @@
+# Load and Plot the IMDB dataset
+import numpy
+from keras.datasets import imdb
+from matplotlib import pyplot
+# load the dataset
+(X_train, y_train), (X_test, y_test) = imdb.load_data(test_split=0)
+# summarize size
+print("Training data: ")
+print(X_train.shape)
+print(y_train.shape)
+# Summarize number of classes
+print("Classes: ")
+print(numpy.unique(y_train))
+# Summarize number of words
+print("Number of words: ")
+print(len(numpy.unique(numpy.hstack(X_train))))
+# Summarize review length
+print("Review length: ")
+result = map(len, X_train)
+print("Mean %.2f words (%f)" % (numpy.mean(result), numpy.std(result)))
+# plot review length as a boxplot and histogram
+pyplot.subplot(121)
+pyplot.boxplot(result)
+pyplot.subplot(122)
+pyplot.hist(result)
+pyplot.show()
